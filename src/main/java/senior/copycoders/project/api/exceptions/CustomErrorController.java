2bb2aa3a8069fbd0dde8cc33e63677a3f3c94fc1
DenisStepanidenko <stepanidenko.den@mail.ru>
2024-08-17 20:17:06 +0300

    fix some bug with error handling

--- src/main/java/senior/copycoders/project/api/exceptions/CustomErrorController.java
+++ src/main/java/senior/copycoders/project/api/exceptions/CustomErrorController.java
@@ -1,14 +1,22 @@
 package senior.copycoders.project.api.exceptions;
 
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.Map;
 
+
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@Controller
 public class CustomErrorController implements ErrorController {
     private static final String PATH = "/error";
 
