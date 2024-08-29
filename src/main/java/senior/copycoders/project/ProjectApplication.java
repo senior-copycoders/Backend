338dbfbe90d0d951676b7fe54ebac98a238ee155
DenisStepanidenko <stepanidenko.den@mail.ru>
2024-08-29 15:17:09 +0300

    start

--- src/main/java/senior/copycoders/project/ProjectApplication.java
+++ src/main/java/senior/copycoders/project/ProjectApplication.java
@@ -2,8 +2,10 @@ package senior.copycoders.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @SpringBootApplication
 public class ProjectApplication {
@@ -14,6 +16,17 @@ public class ProjectApplication {
     }
 
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(urlConfig -> urlConfig
+                        .requestMatchers("/login", "/registration").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
+
+        return http.build();
+    }
 
 }
