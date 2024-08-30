483bde0864ee33d553715b68fc8436e03f16d049
DenisStepanidenko <stepanidenko.den@mail.ru>
2024-08-30 03:17:36 +0300

    continue security

--- src/main/java/senior/copycoders/project/api/exceptions/CustomExceptionHandler.java
+++ src/main/java/senior/copycoders/project/api/exceptions/CustomExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+
 //@Log4j2
 //@ControllerAdvice
 //public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
--- src/main/java/senior/copycoders/project/api/exceptions/GlobalExceptionHandler.java
+++ src/main/java/senior/copycoders/project/api/exceptions/GlobalExceptionHandler.java
@@ -1,27 +1,24 @@
 package senior.copycoders.project.api.exceptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-@Controller
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public ErrorDto handleException(Exception ex) {
-        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR; // По умолчанию
-        String errorDescription = ex.getMessage();
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-        // Проверяем, есть ли у исключения аннотация @ResponseStatus
-        ResponseStatus responseStatus = ex.getClass().getAnnotation(ResponseStatus.class);
-        if (responseStatus != null) {
-            status = responseStatus.value();
-        }
-
-        return new ErrorDto(String.valueOf(status.value()), errorDescription);
-
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorDto> handleNotFoundException(NotFoundException ex) {
+        logger.error("NotFoundException: {}", ex.getMessage());
+        ErrorDto errorDto = new ErrorDto(String.valueOf(HttpStatus.NOT_FOUND.value()), ex.getMessage());
+        return new ResponseEntity<>(errorDto, HttpStatus.NOT_FOUND);
     }
+
 }
